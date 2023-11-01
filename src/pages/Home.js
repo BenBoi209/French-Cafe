8c4e79f29b246fff87c9994ea38b2be8be2a868a
Ben Boi <benboi209@gmail.com>
2023-11-01 21:43:08 +0530

    made-navbar-black

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useCart } from "react-use-cart";
 
 const Home = () => {
+  const { emptyCart } = JSON.parse(localStorage.getItem("react-use-cart"));
   return (
     <div className="home" id="homePage">
       <Link to="/home">
@@ -9,7 +12,11 @@ const Home = () => {
           <h1>Welcome To Our Cafeteria!</h1>
         </div>
         <Link to="/menu">
-          <button type="button" className="btn btn-secondary">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => emptyCart()}
+          >
             Order Now
           </button>
         </Link>
